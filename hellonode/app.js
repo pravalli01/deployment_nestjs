08387bfaff7d928093c91ec41b4a09f5e24ad1f8
pravalli01 <84079448+pravalli01@users.noreply.github.com>
2024-08-01 19:25:35 +0530

    Create app.js

---
+++ hellonode/app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+var express = require('express');
+var app = express();
+const emessage = process.env.MESSAGE;
+
+app.get('/', function (req, res) {
+  res.send(emessage);
+});
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
+
+app.get('/health', (req, res) => {
+  res.send('OK');
+});
