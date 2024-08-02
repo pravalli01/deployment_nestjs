5f2875e0f70622db326b7090bd8b66cfadf50819
pravalli01 <84079448+pravalli01@users.noreply.github.com>
2024-08-02 17:01:13 +0530

    Update app.js

--- hellonode/app.js
+++ hellonode/app.js
@@ -2,22 +2,15 @@ require('dotenv').config();
 
 var express = require('express');
 var app = express();
-const port = process.env.PORT || 8080;
 const emessage = process.env.MESSAGE;
-
-if (!emessage) {
-  console.error('MESSAGE environment variable is not set.');
-  process.exit(1);
-}
 
 app.get('/', function (req, res) {
   res.send(emessage);
 });
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080');
+});
 
 app.get('/health', (req, res) => {
   res.send('OK');
-});
-
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}`);
 });
